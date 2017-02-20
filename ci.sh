f81fb9a89cfde9a4f411e766dada517666fe5b62
Jun Tsai <jcai@ganshane.com>
2017-02-20 17:43:57 +0800

    build iso

--- ci.sh
+++ ci.sh
@@ -6,4 +6,5 @@ git clone --depth 1 https://bitbucket.org/zvirt/zvirt-box.git zvirt-box
 git clone --depth 1 --branch 6.1-Unstable https://github.com/Thinstation/thinstation.git thinstation
 ls -alh
 cp -rp zvirt-box/ts/* thinstation/ts/
-#sudo ./setup-chroot -a -b -o --license ACCEPT --autodl
+cd thinstation
+sudo ./setup-chroot -a -b -o --license ACCEPT --autodl
