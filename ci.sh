2ae11a49a540034d53075c81df8112cbb4a2dd90
Jun Tsai <jcai@ganshane.com>
2017-02-20 20:37:56 +0800

    add ssh key

--- ci.sh
+++ ci.sh
@@ -2,9 +2,16 @@
 set -e
 mkdir target
 cd target
-git clone --depth 1 https://bitbucket.org/zvirt/zvirt-box.git zvirt-box
+echo -e "Host *\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
+echo -e "$SSH_KEY" > ~/.ssh/id_rsa.pub
+echo -e "$SSH_PRIVATE_KEY" > ~/.ssh/id_rsa
+chmod 600 ~/.ssh/id_rsa*
+eval `ssh-agent -s`
+ssh-add ~/.ssh/id_rsa
+
+git clone --depth 1 $GIT_URL zvirt-box
 git clone --depth 1 --branch 6.1-Unstable https://github.com/Thinstation/thinstation.git thinstation
 ls -alh
 cp -rp zvirt-box/ts/* thinstation/ts/
 cd thinstation
-sudo ./setup-chroot -a -b -o --license ACCEPT --autodl
+#sudo ./setup-chroot -a -b -o --license ACCEPT --autodl
