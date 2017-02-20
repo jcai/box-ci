e349e03bd1646d8a879f5b571c0530369bb121f6
Jun Tsai <jcai@ganshane.com>
2017-02-20 17:30:27 +0800

    test directory

--- ci.sh
+++ ci.sh
@@ -3,5 +3,6 @@ mkdir target
 cd target
 git clone --depth 1 https://bitbucket.org/zvirt/zvirt-box.git zvirt-box
 git clone --depth 1 --branch 6.1-Unstable https://github.com/Thinstation/thinstation.git thinstation
+ls -alh
 cp -rp zvirt-box/ts/* thinstation/ts/
 #sudo ./setup-chroot -a -b -o --license ACCEPT --autodl
