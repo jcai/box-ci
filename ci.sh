d71bf937d4c79fa3be8e7816e2a12380032d4b4b
Jun Tsai <jcai@ganshane.com>
2017-02-20 22:45:28 +0800

    add upload script

--- ci.sh
+++ ci.sh
@@ -7,4 +7,7 @@ git clone --depth 1 --branch 6.1-Unstable https://github.com/Thinstation/thinsta
 ls -alh
 cp -rp zvirt-box/ts/* thinstation/ts/
 cd thinstation
-#sudo ./setup-chroot -a -b -o --license ACCEPT --autodl
+sudo ./setup-chroot -a -b -o --license ACCEPT --autodl
+
+cd ../../
+mvn -B deploy  --settings support/settings.xml
