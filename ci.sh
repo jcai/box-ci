c69ff6648f570709c680e9ad2f1e74b7dcc08e04
Jun Tsai <jcai@ganshane.com>
2017-03-05 10:56:02 +0800

    upgrade ci script

--- ci.sh
+++ ci.sh
@@ -4,10 +4,13 @@ mkdir -p target
 cd target
 git clone --depth 1 $GIT_URL zvirt-box
 git clone --depth 1 --branch 6.1-Unstable https://github.com/Thinstation/thinstation.git thinstation
-ls -alh
-cp -rp zvirt-box/ts/* thinstation/ts/
-cd thinstation
-sudo ./setup-chroot -a -b -o --license ACCEPT --autodl
+cd zvirt-box/tools/hard_disk_install
+./shell_build_harddisk_iso.sh ../../../thinstation
+#ls -alh
+#cp -rp zvirt-box/ts/* thinstation/ts/
+#cd thinstation
+#sudo ./setup-chroot -a -b -o --license ACCEPT --autodl
+
 
 cd ../../
 mvn -B deploy  --settings support/settings.xml
